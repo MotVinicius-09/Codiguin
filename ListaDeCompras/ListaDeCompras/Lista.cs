84b2b4689f3992510335b6ddf291c8e034728033
MotVinicius-09 <viniciu.santos12334@gmail.com>
2021-02-07 00:00:41 -0300

    Atualizações

--- ListaDeCompras/ListaDeCompras/Lista.cs
+++ ListaDeCompras/ListaDeCompras/Lista.cs
@@ -9,22 +9,19 @@ namespace ListaDeCompras
         public string ClassProduct { get; set; }
         public int Id { get; set; }
         public double Price {private get; set; }
-        public double TotalPrice { private get;  set; }
+        public double TotalPrice { get;  set; }
         public double QuantProd { get; set; }
 
-        public Lista (string product, string classProduct, int id, double preco, double quantProd)
+        public Lista (string product, string classProduct, int id, double preco, double quantProd,double totalPrice)
         {
             Product = product;
             ClassProduct = classProduct;
             Id = id;
             Price = preco;
             QuantProd = quantProd;
+            TotalPrice = totalPrice;
         }
-        
-        public void ValorTotal()
-        {
-            TotalPrice = Price * TotalPrice;
-        }
+
         public override string ToString()
         {
             return "Id: " + Id
--- ListaDeCompras/ListaDeCompras/Program.cs
+++ ListaDeCompras/ListaDeCompras/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace ListaDeCompras
 {
@@ -9,53 +10,69 @@ namespace ListaDeCompras
         static void Main(string[] args)
         {
             Console.WriteLine("----LISTA-DE-COMPRAS----");
-                Console.Write("Quantos produtos você vai levar?: ");
-                int quantItem = int.Parse(Console.ReadLine());
+            Console.WriteLine("Itens no estoque: Alimento,Bebida,Limpeza e Fruta");
+            Console.WriteLine();
+
+            Console.Write("Quantos produtos você vai levar?: ");
+            int quantItem = int.Parse(Console.ReadLine());
 
             List<Lista> listaDeCompras = new List<Lista>(quantItem);
-            
-            
-            for(int i = 0; i < quantItem; i++)
+
+
+            for (int i = 0; i < quantItem; i++)
             {
                 Random idAleat = new Random();
                 int id = idAleat.Next(100, 999);
 
                 Console.WriteLine();
-                Console.WriteLine("Digite o " + (i + 1)  + "°" +"Item:");
+                Console.WriteLine("Digite o " + (i + 1) + "°" + "Item:");
                 Console.Write("Produto: ");
                 string produto = Console.ReadLine();
-                
+
                 Console.Write("Tipo do produto: ");
                 string tipoDoProduto = Console.ReadLine();
 
                 Console.Write("Quantidade do produto: ");
                 double quantProd = double.Parse(Console.ReadLine());
-                   
+
                 double preco = 0;
-                switch(tipoDoProduto)
+                double precoFinal = 0;
+                switch (tipoDoProduto)
                 {
                     case "Alimento":
                         preco = 3.00;
+                        precoFinal = preco * quantProd;
                         break;
                     case "Limpeza":
                         preco = 5.00;
+                        precoFinal = preco * quantProd;
                         break;
                     case "Fruta":
                         preco = 1.00;
+                        precoFinal = preco * quantProd;
                         break;
                     case "Bebida":
                         preco = 2.00;
+                        precoFinal = preco * quantProd;
                         break;
                 }
-                listaDeCompras.Add(new Lista(produto, tipoDoProduto,id,preco,quantProd));
+                listaDeCompras.Add(new Lista(produto, tipoDoProduto, id, preco, quantProd, precoFinal));
             }
 
+            Console.Clear();
             Console.WriteLine();
             List<Lista> listaAlimentos = listaDeCompras.FindAll(x => x.ClassProduct == "Alimento");
             Console.WriteLine("----Lista de Alimento----");
-            foreach(Lista p in listaAlimentos)
+            foreach (Lista p in listaAlimentos)
             {
-                Console.WriteLine(p);
+                if (listaAlimentos != null)
+                {
+                    Console.WriteLine(p);
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
             }
 
             Console.WriteLine();
@@ -63,9 +80,10 @@ namespace ListaDeCompras
             Console.WriteLine("--Lista de Bebidas--");
             foreach (Lista p in listaBebidas)
             {
+
                 Console.WriteLine(p);
             }
-            
+
             Console.WriteLine();
             List<Lista> listaLimpeza = listaDeCompras.FindAll(x => x.ClassProduct == "Limpeza");
             Console.WriteLine("--Lista de Limpeza--");
@@ -76,11 +94,18 @@ namespace ListaDeCompras
 
             Console.WriteLine();
             List<Lista> listaFruta = listaDeCompras.FindAll(x => x.ClassProduct == "Fruta");
-            Console.WriteLine("--Lista de Fruta--");
+            Console.WriteLine("--Lista de Limpeza--");
             foreach (Lista p in listaFruta)
             {
                 Console.WriteLine(p);
             }
+
+            Console.WriteLine();
+            Console.WriteLine("--------------Total-a-pagar---------------");
+            int soma = listaDeCompras.Sum(x => Convert.ToInt32(x.TotalPrice));
+            double total = Convert.ToDouble(soma);
+            Console.WriteLine("Total a pagar: R$" + total.ToString("F2"));
         }
+
     }
 }
